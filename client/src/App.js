bbba17d1575e048a69d0fc42242bf9b2a922579a
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-07 15:38:37 -0700

    created exact path to route to main page, in this case movie list

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
       </div>
     );
   }
